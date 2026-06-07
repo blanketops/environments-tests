7a0e9d992fd210ed346931a1eb36f9205dee8351
Neo Tlaletsi <ntlaletsi86@gmail.com>
2026-06-07 17:57:41 +0200

    Update gitrepository.go

--- tests/data/sources/gitrepository.go
+++ tests/data/sources/gitrepository.go
@@ -1,19 +1,37 @@
-type GitRepository struct{
+/*
+Copyright 2025 The BlanketOps Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package environments
+
+type GitRepositoryData struct{
+   
    Provider string
    HookUrl string
    Repository Repository
    Webhooks Webooks
 }
 
-type Repository {
+type Repository struct {
   Owner string
   Name string
 }
 
-type Webhooks {
+type Webhooks struct {
   Events[] Events
 }
 
-type Events{
+type Events struct{
   Event string
 }
