5402c1d23ae9b4a6f2eb819bfa80d4c9623ab3b3
Neo Tlaletsi <ntlaletsi86@gmail.com>
2026-06-07 17:52:12 +0200

    Update gitrepository.go

--- tests/data/sources/gitrepository.go
+++ tests/data/sources/gitrepository.go
@@ -1,1 +1,19 @@
+type GitRepository struct{
+   Provider string
+   HookUrl string
+   Repository Repository
+   Webhooks Webooks
+}
 
+type Repository {
+  Owner string
+  Name string
+}
+
+type Webhooks {
+  Events[] Events
+}
+
+type Events{
+  Event string
+}
